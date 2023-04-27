5dc5951e0e7a0c450ca6e7a85cf0238f4f121836
김재원 <jw.kim@sirloin.co.kr>
2023-04-27 12:15:10 +0900

    Update 등록 페이지 > form, input 추가

--- src/pages/workout/create/index.tsx
+++ src/pages/workout/create/index.tsx
@@ -1,9 +1,21 @@
 import Link from 'next/link';
+import { FormEvent } from 'react';
 
 export default function WorkoutCreate() {
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    alert((e.target as any).name.value);
+  }
+
   return (
     <main>
       <h1>등록 페이지</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
       <Link href="/">메인 페이지로</Link>
     </main>
   );
