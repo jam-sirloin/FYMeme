e338cce0d6294ded7ddebcffe042fd6b6cf72396
김재원 <jw.kim@sirloin.co.kr>
2023-05-18 17:10:12 +0900

    Update 메인 페이지 > localStorage의 데이터 렌더링

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,28 @@
 import Link from 'next/link';
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 
 export default function Home() {
   useEffect(() => {
     console.log(localStorage.getItem('myWorkoutList'));
   }, []);
 
+  const workoutList = useMemo(() => {
+    const myWorkoutList =
+      typeof window !== 'undefined'
+        ? localStorage.getItem('myWorkoutList')
+        : null;
+
+    return myWorkoutList ? JSON.parse(myWorkoutList) : [];
+  }, []);
+
   return (
     <main>
       <h1>메인 페이지</h1>
+      <ul>
+        {workoutList.map((workoutName: string, index: number) => (
+          <li key={index}>{workoutName}</li>
+        ))}
+      </ul>
       <Link href="/workout/create">등록 페이지로</Link>
     </main>
   );
