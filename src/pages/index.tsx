0f27ce9aca1a4c43a46b1e7857b67a19d806c3cb
김재원 <jw.kim@sirloin.co.kr>
2023-05-23 16:05:21 +0900

    Fix 메인 페이지 > Hydration 문제 해결 * 브라우저의 첫 번째 렌더링 전에 useMemo의 반환값이 변경되지 않도록 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,33 @@
 import Link from 'next/link';
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 export default function Home() {
+  const [mounted, setMounted] = useState(false);
+
   useEffect(() => {
+    setMounted(true);
     console.log(localStorage.getItem('myWorkoutList'));
   }, []);
 
   const workoutList = useMemo(() => {
+    if (!mounted) return null; // to solve hydration error
+
     const myWorkoutList =
       typeof window !== 'undefined'
         ? localStorage.getItem('myWorkoutList')
         : null;
 
     return myWorkoutList ? JSON.parse(myWorkoutList) : [];
-  }, []);
+  }, [mounted]);
 
   return (
     <main>
       <h1>메인 페이지</h1>
       <ul>
-        {workoutList.map((workoutName: string, index: number) => (
-          <li key={index}>{workoutName}</li>
-        ))}
+        {workoutList &&
+          workoutList.map((workoutName: string, index: number) => (
+            <li key={index}>{workoutName}</li>
+          ))}
       </ul>
       <Link href="/workout/create">등록 페이지로</Link>
     </main>
